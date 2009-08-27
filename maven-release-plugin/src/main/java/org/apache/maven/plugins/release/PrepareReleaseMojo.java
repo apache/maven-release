2c58916749264e9cc2ff3015fc8f4f8c3815e645
Olivier Lamy <olamy@apache.org>
2009-08-27 23:22:58 +0000

    prefer session.getExecutionProperties() than System.getProperties() (as it can cause issue in a multithreaded env)
    git-svn-id: https://svn.apache.org/repos/asf/maven/release/trunk@808677 13f79535-47bb-0310-9956-ffa450edef68

--- maven-release-plugin/src/main/java/org/apache/maven/plugins/release/PrepareReleaseMojo.java
+++ maven-release-plugin/src/main/java/org/apache/maven/plugins/release/PrepareReleaseMojo.java
@@ -19,6 +19,7 @@ package org.apache.maven.plugins.release;
  * under the License.
  */
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.shared.release.ReleaseExecutionException;
@@ -151,6 +152,14 @@ public class PrepareReleaseMojo
     private boolean remoteTagging;
     
     /**
+     * @parameter expression="${session}"
+     * @readonly
+     * @required
+     * @since 2.0-beta-10
+     */
+    protected MavenSession session;    
+    
+    /**
      * {@inheritDoc}
      */
     public void execute()
@@ -172,9 +181,9 @@ public class PrepareReleaseMojo
         config.setDefaultDevelopmentVersion( developmentVersion );
         config.setRemoteTagging( remoteTagging );
 
-        // Create a config containing values from the system properties (command line properties).
+        // Create a config containing values from the session properties (ie command line properties with cli).
         ReleaseDescriptor sysPropertiesConfig
-                = ReleaseUtils.copyPropertiesToReleaseDescriptor( System.getProperties() );
+                = ReleaseUtils.copyPropertiesToReleaseDescriptor( session.getExecutionProperties() );
         mergeCommandLineConfig( config, sysPropertiesConfig );
 
         try
