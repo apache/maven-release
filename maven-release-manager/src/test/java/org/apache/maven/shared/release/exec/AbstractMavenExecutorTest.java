e12b4cb418f3329659ee784851d396aad98d19f8
Robert Scholte <rfscholte@apache.org>
2012-03-22 16:49:30 +0000

    How weird, IDE had no problem to run this test, surefire did. Change visibility to please surefire
    git-svn-id: https://svn.apache.org/repos/asf/maven/release/trunk@1303887 13f79535-47bb-0310-9956-ffa450edef68

--- maven-release-manager/src/test/java/org/apache/maven/shared/release/exec/AbstractMavenExecutorTest.java
+++ maven-release-manager/src/test/java/org/apache/maven/shared/release/exec/AbstractMavenExecutorTest.java
@@ -94,7 +94,7 @@ public class AbstractMavenExecutorTest
         reset( executor );
     }
 
-    private class AbstractMavenExecutorSpy
+    protected class AbstractMavenExecutorSpy
         extends AbstractMavenExecutor
     {
 
