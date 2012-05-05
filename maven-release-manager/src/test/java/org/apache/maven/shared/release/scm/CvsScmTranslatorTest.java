87fc1af7e41318b57058128c9e1f4b8fe8a9eddb
Robert Scholte <rfscholte@apache.org>
2012-05-05 10:01:51 +0000

    Add CvsScmTranslatorTest
    git-svn-id: https://svn.apache.org/repos/asf/maven/release/trunk@1334373 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-release-manager/src/test/java/org/apache/maven/shared/release/scm/CvsScmTranslatorTest.java
@@ -0,0 +1,50 @@
+package org.apache.maven.shared.release.scm;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+
+public class CvsScmTranslatorTest
+    extends TestCase
+{
+    private CvsScmTranslator scmTranslator = new CvsScmTranslator();   
+    
+    /**
+     * @see org.apache.maven.model.Scm#getTag()
+     */
+    public void testResolveTag() 
+    {
+        // with current implementation you can't call your tag 'HEAD' (which is the default)
+        assertEquals( null, scmTranslator.resolveTag( "HEAD" ) );
+        assertEquals( "project-1.0", scmTranslator.resolveTag( "project-1.0" ) );
+    }
+
+    public void testTranslateTagUrl()
+    {
+        assertEquals( "url", scmTranslator.translateTagUrl( "url", "tag", null )  );
+        assertEquals( "url", scmTranslator.translateTagUrl( "url", "tag", "tagBase" )  );
+    }
+
+    public void testTranslateBranchUrl()
+    {
+        assertEquals( "url", scmTranslator.translateBranchUrl( "url", "branchName", null )  );
+        assertEquals( "url", scmTranslator.translateBranchUrl( "url", "branchName", "tagBase" )  );
+    }
+}
