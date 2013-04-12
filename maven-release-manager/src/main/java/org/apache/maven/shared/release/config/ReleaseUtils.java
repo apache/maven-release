b84fae76d1890e7aae02c287b522dfcfb046593b
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-04-12 21:52:08 +0200

    MRELEASE-834: Add fix.

--- maven-release-manager/src/main/java/org/apache/maven/shared/release/config/ReleaseUtils.java
+++ maven-release-manager/src/main/java/org/apache/maven/shared/release/config/ReleaseUtils.java
@@ -253,6 +253,13 @@ public class ReleaseUtils
                     versionType = ReleaseDescriptor.RELEASE_KEY;
                 }
 
+                // system property which is *not* a dependency, see
+                // http://jira.codehaus.org/browse/MRELEASE-834
+                if ( endIndex == -1 )
+                {
+                    continue;
+                }
+
                 artifactVersionlessKey = propertyName.substring( startIndex, endIndex );
 
                 if ( resolvedDependencies.containsKey( artifactVersionlessKey ) )
