4375d35f325dbb19b13b071c2c93610b3e4b74e0
Guillaume Boué <gboue@apache.org>
2017-01-09 01:21:56 +0000

    [MRELEASE-976] release:branch should also support project version policies
    Adding parameter projectVersionPolicyId to the branch and update-versions goals. The ITs are done by creating a stub version policy that switches between snapshot and release and testing that it is possible to invoke release:branch, release:update-versions and release:prepare on a sample project with this custom policy. This closes #9.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/release/trunk@1777925 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-release-plugin/src/it/projects/prepare/MRELEASE-976/verify.groovy
@@ -0,0 +1,30 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+def project = new XmlSlurper().parse( new File( basedir, "pom.xml" ) )
+assert project.version.text() == "1.0-SNAPSHOT"
+
+def projectTag = new XmlSlurper().parse( new File( basedir, "pom.xml.tag" ) )
+assert projectTag.version.text() == "1.0"
+
+def projectNext = new XmlSlurper().parse( new File( basedir, "pom.xml.next" ) )
+assert projectNext.version.text() == "1.0-SNAPSHOT"
+
+return true
