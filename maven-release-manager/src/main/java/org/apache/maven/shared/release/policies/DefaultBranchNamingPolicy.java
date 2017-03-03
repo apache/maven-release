85c82718e0953d6fd378dcc77622b2e2ae346ae7
Henning Schmiedehausen <henning.schmiedehausen@zuora.com>
2017-03-03 10:37:56 -0800

    Adds a second policy for branch naming as compared to tag naming.
    Use different configuration settings for branch and tag naming. Add a second default policy that is used
    when naming branches.

---
+++ maven-release-manager/src/main/java/org/apache/maven/shared/release/policies/DefaultBranchNamingPolicy.java
@@ -0,0 +1,41 @@
+package org.apache.maven.shared.release.policies;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.shared.release.policy.PolicyException;
+import org.apache.maven.shared.release.policy.naming.NamingPolicy;
+import org.apache.maven.shared.release.policy.naming.NamingPolicyRequest;
+import org.apache.maven.shared.release.policy.naming.NamingPolicyResult;
+import org.codehaus.plexus.component.annotations.Component;
+
+/**
+ *
+ * @since 3.0.0
+ */
+@Component( role = NamingPolicy.class, hint = "default-branch" )
+public class DefaultBranchNamingPolicy implements NamingPolicy
+{
+    @Override
+    public NamingPolicyResult getName( NamingPolicyRequest request )
+        throws PolicyException
+    {
+        return new NamingPolicyResult().setName( null );
+    }
+}
