820e774bbe892d2f1f0d77b8355216ad87d264ec
Tamas Cservenak <tamas@cservenak.net>
2022-05-05 09:29:22 +0200

    Fix deprecations related to CLI

--- maven-release-manager/src/main/java/org/apache/maven/shared/release/exec/InvokerMavenExecutor.java
+++ maven-release-manager/src/main/java/org/apache/maven/shared/release/exec/InvokerMavenExecutor.java
@@ -31,9 +31,9 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.PosixParser;
 import org.apache.maven.settings.io.xpp3.SettingsXpp3Writer;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
@@ -54,36 +54,35 @@ import org.slf4j.Logger;
  */
 @Singleton
 @Named( "invoker" )
-@SuppressWarnings( "static-access" )
 public class InvokerMavenExecutor
         extends AbstractMavenExecutor
 {
 
     private static final Options OPTIONS = new Options();
 
-    private static final char SET_SYSTEM_PROPERTY = 'D';
+    private static final String SET_SYSTEM_PROPERTY = "D";
 
-    private static final char OFFLINE = 'o';
+    private static final String OFFLINE = "o";
 
-    private static final char REACTOR = 'r';
+    private static final String REACTOR = "r";
 
-    private static final char QUIET = 'q';
+    private static final String QUIET = "q";
 
-    private static final char DEBUG = 'X';
+    private static final String DEBUG = "X";
 
-    private static final char ERRORS = 'e';
+    private static final String ERRORS = "e";
 
-    private static final char NON_RECURSIVE = 'N';
+    private static final String NON_RECURSIVE = "N";
 
-    private static final char UPDATE_SNAPSHOTS = 'U';
+    private static final String UPDATE_SNAPSHOTS = "U";
 
-    private static final char ACTIVATE_PROFILES = 'P';
+    private static final String ACTIVATE_PROFILES = "P";
 
-    private static final char CHECKSUM_FAILURE_POLICY = 'C';
+    private static final String CHECKSUM_FAILURE_POLICY = "C";
 
-    private static final char CHECKSUM_WARNING_POLICY = 'c';
+    private static final String CHECKSUM_WARNING_POLICY = "c";
 
-    private static final char ALTERNATE_USER_SETTINGS = 's';
+    private static final String ALTERNATE_USER_SETTINGS = "s";
 
     private static final String ALTERNATE_GLOBAL_SETTINGS = "gs";
 
@@ -99,79 +98,137 @@ public class InvokerMavenExecutor
 
     private static final String BATCH_MODE = "B";
 
-    /**
-     * Constant <code>ALTERNATE_USER_TOOLCHAINS='t'</code>
-     */
-    public static final char ALTERNATE_USER_TOOLCHAINS = 't';
+    public static final String ALTERNATE_USER_TOOLCHAINS = "t";
 
     static
     {
         OPTIONS.addOption(
-                OptionBuilder.withLongOpt( "define" ).hasArg().withDescription( "Define a system property" ).create(
-                        SET_SYSTEM_PROPERTY ) );
-
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "offline" ).withDescription( "Work offline" ).create( OFFLINE ) );
-
-        OPTIONS.addOption(
-                OptionBuilder.withLongOpt( "quiet" ).withDescription( "Quiet output - only show errors" )
-                        .create( QUIET ) );
+                Option.builder( SET_SYSTEM_PROPERTY )
+                        .longOpt( "define" )
+                        .numberOfArgs( 2 )
+                        .valueSeparator( '=' )
+                        .desc( "Define a system property" )
+                        .build() );
 
         OPTIONS.addOption(
-                OptionBuilder.withLongOpt( "debug" ).withDescription( "Produce execution debug output" )
-                        .create( DEBUG ) );
+                Option.builder( OFFLINE )
+                        .longOpt( "offline" )
+                        .desc( "Work offline" )
+                        .build() );
 
         OPTIONS.addOption(
-                OptionBuilder.withLongOpt( "errors" ).withDescription( "Produce execution error messages" ).create(
-                        ERRORS ) );
-
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "reactor" ).withDescription(
-                "Execute goals for project found in the reactor" ).create( REACTOR ) );
-
-        OPTIONS.addOption(
-                OptionBuilder.withLongOpt( "non-recursive" ).withDescription( "Do not recurse into sub-projects" )
-                        .create(
-                                NON_RECURSIVE ) );
-
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "update-snapshots" ).withDescription(
-                        "Forces a check for updated releases and snapshots on remote repositories" )
-                .create( UPDATE_SNAPSHOTS ) );
-
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "activate-profiles" ).withDescription(
-                "Comma-delimited list of profiles to activate" ).hasArg().create( ACTIVATE_PROFILES ) );
-
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "strict-checksums" ).withDescription(
-                "Fail the build if checksums don't match" ).create( CHECKSUM_FAILURE_POLICY ) );
+                Option.builder( QUIET )
+                        .longOpt( "quiet" )
+                        .desc( "Quiet output - only show errors" )
+                        .build() );
 
         OPTIONS.addOption(
-                OptionBuilder.withLongOpt( "lax-checksums" ).withDescription( "Warn if checksums don't match" ).create(
-                        CHECKSUM_WARNING_POLICY ) );
+                Option.builder( DEBUG )
+                        .longOpt( "debug" )
+                        .desc( "Produce execution debug output" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "settings" ).withDescription(
-                "Alternate path for the user settings file" ).hasArg().create( ALTERNATE_USER_SETTINGS ) );
+        OPTIONS.addOption(
+                Option.builder( ERRORS )
+                        .longOpt( "errors" )
+                        .desc( "Produce execution error messages" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "global-settings" ).withDescription(
-                " Alternate path for the global settings file" ).hasArg().create( ALTERNATE_GLOBAL_SETTINGS ) );
+        OPTIONS.addOption(
+                Option.builder( REACTOR )
+                        .longOpt( "reactor" )
+                        .desc( "Execute goals for project found in the reactor" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "fail-fast" ).withDescription(
-                "Stop at first failure in reactorized builds" ).create( FAIL_FAST ) );
+        OPTIONS.addOption(
+                Option.builder( NON_RECURSIVE )
+                        .longOpt( "non-recursive" )
+                        .desc( "Do not recurse into sub-projects" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "fail-at-end" ).withDescription(
-                "Only fail the build afterwards; allow all non-impacted builds to continue" ).create( FAIL_AT_END ) );
+        OPTIONS.addOption(
+                Option.builder( UPDATE_SNAPSHOTS )
+                        .longOpt( "update-snapshots" )
+                        .desc( "Forces a check for updated releases and snapshots on remote repositories" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "fail-never" ).withDescription(
-                "NEVER fail the build, regardless of project result" ).create( FAIL_NEVER ) );
+        OPTIONS.addOption(
+                Option.builder( ACTIVATE_PROFILES )
+                        .longOpt( "activate-profiles" )
+                        .desc( "Comma-delimited list of profiles to activate" )
+                        .hasArg()
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "file" ).withDescription(
-                "Force the use of an alternate POM file." ).hasArg().create( ALTERNATE_POM_FILE ) );
+        OPTIONS.addOption(
+                Option.builder( CHECKSUM_FAILURE_POLICY )
+                        .longOpt( "strict-checksums" )
+                        .desc( "Fail the build if checksums don't match" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "threads" ).withDescription(
-                "Thread count, for instance 2.0C where C is core multiplied" ).hasArg().create( THREADS ) );
+        OPTIONS.addOption(
+                Option.builder( CHECKSUM_WARNING_POLICY )
+                        .longOpt( "lax-checksums" )
+                        .desc( "Warn if checksums don't match" )
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "batch-mode" ).withDescription(
-                "Run in non-interactive (batch) mode" ).create( BATCH_MODE ) );
+        OPTIONS.addOption(
+                Option.builder( ALTERNATE_USER_SETTINGS )
+                        .longOpt( "settings" )
+                        .desc( "Alternate path for the user settings file" )
+                        .hasArg()
+                        .build() );
 
-        OPTIONS.addOption( OptionBuilder.withLongOpt( "toolchains" ).withDescription(
-                "Alternate path for the user toolchains file" ).hasArg().create( ALTERNATE_USER_TOOLCHAINS ) );
+        OPTIONS.addOption(
+                Option.builder( ALTERNATE_GLOBAL_SETTINGS )
+                        .longOpt( "global-settings" )
+                        .desc( "Alternate path for the global settings file" )
+                        .hasArg()
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( FAIL_FAST )
+                        .longOpt( "fail-fast" )
+                        .desc( "Stop at first failure in reactorized builds" )
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( FAIL_AT_END )
+                        .longOpt( "fail-at-end" )
+                        .desc( "Only fail the build afterwards; allow all non-impacted builds to continue" )
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( FAIL_NEVER )
+                        .longOpt( "fail-never" )
+                        .desc( "NEVER fail the build, regardless of project result" )
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( ALTERNATE_POM_FILE )
+                        .longOpt( "file" )
+                        .desc( "Force the use of an alternate POM file." )
+                        .hasArg()
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( THREADS )
+                        .longOpt( "threads" )
+                        .desc( "Thread count, for instance 2.0C where C is core multiplied" )
+                        .hasArg()
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( BATCH_MODE )
+                        .longOpt( "batch-mode" )
+                        .desc( "Run in non-interactive (batch) mode" )
+                        .build() );
+
+        OPTIONS.addOption(
+                Option.builder( ALTERNATE_USER_TOOLCHAINS )
+                        .longOpt( "toolchains" )
+                        .desc( "Alternate path for the user toolchains file" )
+                        .hasArg()
+                        .build() );
     }
 
     @Inject
@@ -198,15 +255,14 @@ public class InvokerMavenExecutor
         try
         {
             String[] args = CommandLineUtils.translateCommandline( additionalArguments );
-            CommandLine cli = new PosixParser().parse( OPTIONS, args );
+            CommandLine cli = new DefaultParser().parse( OPTIONS, args );
 
             if ( cli.hasOption( SET_SYSTEM_PROPERTY ) )
             {
                 String[] properties = cli.getOptionValues( SET_SYSTEM_PROPERTY );
                 Properties props = new Properties();
-                for ( int i = 0; i < properties.length; i++ )
+                for ( String property : properties )
                 {
-                    String property = properties[i];
                     String name, value;
                     int sep = property.indexOf( "=" );
                     if ( sep <= 0 )
@@ -421,7 +477,7 @@ public class InvokerMavenExecutor
                 if ( invocationResult.getExitCode() != 0 )
                 {
                     throw new MavenExecutorException(
-                            "Maven execution failed, exit code: \'" + invocationResult.getExitCode() + "\'",
+                            "Maven execution failed, exit code: '" + invocationResult.getExitCode() + "'",
                             invocationResult.getExitCode() );
                 }
             }
@@ -462,7 +518,7 @@ public class InvokerMavenExecutor
     private static final class Handler
             implements InvocationOutputHandler
     {
-        private Logger logger;
+        private final Logger logger;
 
         Handler( Logger logger )
         {
@@ -479,7 +535,7 @@ public class InvokerMavenExecutor
             implements InvokerLogger
     {
 
-        private Logger logger;
+        private final Logger logger;
 
         LoggerBridge( Logger logger )
         {
