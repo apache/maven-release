4433a8f3d8772ec5111cd927aac2c379c1977361
Konrad Windszus <kwin@apache.org>
2024-02-02 17:52:29 +0100

    [MRELEASE-1139] Improve logging of sources for used credentials
    Clarify precedence of credentials resolved via settings and those
    explicitly given.

--- maven-release-manager/src/main/java/org/apache/maven/shared/release/scm/DefaultScmRepositoryConfigurator.java
+++ maven-release-manager/src/main/java/org/apache/maven/shared/release/scm/DefaultScmRepositoryConfigurator.java
@@ -96,6 +96,9 @@ public class DefaultScmRepositoryConfigurator implements ScmRepositoryConfigurat
 
             if (releaseDescriptor.getScmId() != null) {
                 server = settings.getServer(releaseDescriptor.getScmId());
+                if (server == null) {
+                    logger.warn("No server with id {} found in Maven settings", releaseDescriptor.getScmId());
+                }
             }
 
             if (server == null && repository.getProviderRepository() instanceof ScmProviderRepositoryWithHost) {
@@ -115,19 +118,28 @@ public class DefaultScmRepositoryConfigurator implements ScmRepositoryConfigurat
             }
 
             if (server != null) {
-                if (username == null) {
+                if (username == null && server.getUsername() != null) {
+                    logger.debug(
+                            "Using username from server id {} found in Maven settings", releaseDescriptor.getScmId());
                     username = server.getUsername();
                 }
 
-                if (password == null) {
+                if (password == null && server.getPassword() != null) {
+                    logger.debug(
+                            "Using password from server id {} found in Maven settings", releaseDescriptor.getScmId());
                     password = decrypt(server.getPassword(), server.getId());
                 }
 
-                if (privateKey == null) {
+                if (privateKey == null && server.getPrivateKey() != null) {
+                    logger.debug(
+                            "Using private key from server id {} found in Maven settings",
+                            releaseDescriptor.getScmId());
                     privateKey = server.getPrivateKey();
                 }
 
-                if (passphrase == null) {
+                if (passphrase == null && server.getPassphrase() != null) {
+                    logger.debug(
+                            "Using passphrase from server id {} found in Maven settings", releaseDescriptor.getScmId());
                     passphrase = decrypt(server.getPassphrase(), server.getId());
                 }
             }
@@ -135,19 +147,27 @@ public class DefaultScmRepositoryConfigurator implements ScmRepositoryConfigurat
 
         if (!(username == null || username.isEmpty())) {
             scmRepo.setUser(username);
+        } else {
+            logger.debug("No explicit username configured");
         }
         if (!(password == null || password.isEmpty())) {
             scmRepo.setPassword(password);
+        } else {
+            logger.debug("No explicit password configured");
         }
 
         if (scmRepo instanceof ScmProviderRepositoryWithHost) {
             ScmProviderRepositoryWithHost repositoryWithHost = (ScmProviderRepositoryWithHost) scmRepo;
             if (!(privateKey == null || privateKey.isEmpty())) {
                 repositoryWithHost.setPrivateKey(privateKey);
+            } else {
+                logger.debug("No explicit private key configured");
             }
 
             if (!(passphrase == null || passphrase.isEmpty())) {
                 repositoryWithHost.setPassphrase(passphrase);
+            } else {
+                logger.debug("No explicit passphrase configured");
             }
         }
 
